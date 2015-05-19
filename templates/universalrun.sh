fb7db83f3a390ca9b9b1b89c13f42439b56435e6
Роман Беляковский <ihryamzik@gmail.com>
2015-05-20 01:26:55 +0300

    moved arch-specific run scripts to separate folders, added bin cleanup

--- templates/universalrun.sh
+++ templates/universalrun.sh
@@ -7,6 +7,8 @@ else
   cd /compile/openwrt-{{ unirun_open_wrt_release_version }} && su compile -c "time make defconfig" 2>&1 | tee    /compile/logs/make.log
 fi
 
+rm -rf /compile/openwrt-{{ unirun_open_wrt_release_version }}/bin/{{ unirun_open_wrt_arch }}
+
 chown compile:compile /compile/openwrt-{{ unirun_open_wrt_release_version }}/bin
 cd /compile/openwrt-{{ unirun_open_wrt_release_version }} && su compile -c "time make V=99"      2>&1 | tee -a /compile/logs/make.log
 date > /compile/logs/openwrt-{{ unirun_open_wrt_release_version }}-builder-raw.done
